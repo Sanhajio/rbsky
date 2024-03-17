8707f630c06b4a62055a926d33db88118781d1d3
Sanhaji Omar <omar.sanhaji@easybourse.com>
2024-03-17 16:53:20 +0100

    Adding logging to file

--- src/bin/nvim.rs
+++ src/bin/nvim.rs
@@ -1,13 +1,32 @@
-use atrium_api::app::bsky::feed::defs::FeedViewPost;
-use env_logger;
-use log::trace;
+use anyhow::{Context, Result};
+use log::{info, trace};
 use rbsky::{nvim::EventHandler, surreal::SurrealDB};
+use simple_log::LogConfigBuilder;
+use tokio::fs::create_dir_all;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     // TODO:: Add a file logger for now in config dir, rbsky-nvim.log
     // TODO:: Start a process to update the data stored in surrealdb
-    env_logger::init();
+    let config_dir =
+        dirs::config_dir().with_context(|| format!("No config dir: {:?}", dirs::config_dir()))?;
+    let dir = config_dir.join("bsky");
+    create_dir_all(&dir).await?;
+    let path = dir.join("rbsky.log");
+    if let Some(path) = path.to_str() {
+        let config = LogConfigBuilder::builder()
+            .path(String::from(path))
+            .level("trace")
+            .size(1 * 100)
+            .roll_count(10)
+            .output_file()
+            .output_console()
+            .build();
+        let _ = simple_log::new(config);
+    } else {
+        env_logger::init();
+    }
+    info!("Logger Initialized");
     let db = SurrealDB::new().await?;
     let mut event_handler = EventHandler::new(db)?;
     event_handler.recv().await?;
--- src/nvim.rs
+++ src/nvim.rs
@@ -26,6 +26,8 @@ impl EventHandler {
         Ok(EventHandler { nvim, db })
     }
 
+    // TODO: add args to the recv function, add timeline, which timeline should I read
+    // Add this in the setup of the binary adding clap
     pub async fn recv(&mut self) -> Result<(), anyhow::Error> {
         let receiver = self.nvim.session.start_event_loop_channel();
         let mut neosky_buffer: Option<Buffer> = None;
@@ -33,36 +35,10 @@ impl EventHandler {
             trace!("Received event: {:?}, values: {:?}", event, values);
             match Messages::from(event) {
                 Messages::Read => {
-                    // TODO:: I might not need to open a buffer to enter the data there
-                    // I would rather take the data into a table and display it from neovim
                     let cached_feed: Vec<FeedViewPost> =
                         self.db.read_timeline(String::from("default")).await?;
                     trace!("Reading the data: {:?}", cached_feed);
-                    let buffers: Vec<Buffer> = self.nvim.list_bufs()?;
-                    for buf in buffers {
-                        let name = buf.get_name(&mut self.nvim)?;
-                        if name.ends_with("neosky.social") {
-                            neosky_buffer = Some(buf);
-                            break;
-                        }
-                    }
-                    if neosky_buffer.is_none() {
-                        self.nvim.command("enew")?;
-                        self.nvim.command("file neosky.social")?;
-                        neosky_buffer = Some(self.nvim.get_current_buf()?)
-                    }
-                    if let Some(ref buffer) = neosky_buffer {
-                        self.nvim.set_current_buf(&buffer)?;
-
-                        // TODO:: send a table data to feed
-                        let feed_lines: Vec<String> = cached_feed
-                            .iter()
-                            .map(|post| serde_json::to_string(post).unwrap())
-                            .collect();
-
-                        // TODO::
-                        buffer.set_lines(&mut self.nvim, 0, -1, true, feed_lines)?;
-                    }
+                    // self.nvim.call_function(, args)
                 }
                 Messages::Post => {
                     // TODO:: Add an nui or any other ui plugin to add a Post like interface
